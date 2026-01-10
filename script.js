29c03eecfada62599d38794ef5ee290ddd2b40ef
yehtennis-cloud <yehtennis@gmail.com>
2026-01-10 01:41:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const SUPABASE_URL = "https://sdicmtmcanvswsisihqb.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_pAMpbQ_ZpucKn9X8xgQUdA_as-rPsa7";
 
-const supabase = supabase.createClient(
+const supabaseClient = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
